61fb376e836ebe91474a08ffc84c6ba6b1089bf8
chunghanlee1 <chunghan.lee1@gmail.com>
2019-08-01 12:30:21 +0100

    fixed linting problem

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -18,7 +18,7 @@ class CommandTests(TestCase):
         # and replace it with a mock object that returns what we want
         # and monitors the number of calls being made to it
         with patch('django.db.utils.ConnectionHandler.__getitem__') as \
-        get_item:
+                get_item:
             # instruct this mock to always return true
             get_item.return_value = True
             # wait_for_db is the command we will create
@@ -36,7 +36,7 @@ class CommandTests(TestCase):
     def test_wait_for_db(self, time_sleep):
         """Test waiting for db until the db is ready"""
         with patch('django.db.utils.ConnectionHandler.__getitem__') as \
-        get_item:
+                get_item:
             # Make the __getitem__ method return Error the first five times
             get_item.side_effect = [OperationalError] * 5 + [True]
             call_command('wait_for_db')
