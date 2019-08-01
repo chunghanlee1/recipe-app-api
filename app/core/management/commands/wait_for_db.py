636710ea88b0db4f5ca459ea2f3195a547597e72
chunghanlee1 <chunghan.lee1@gmail.com>
2019-08-01 17:23:08 +0800

    Edit test db wait connection for linting

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -5,12 +5,13 @@ from django.db.utils import OperationalError
 # Base command class for us to build custom management commands
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
     """Django command to pause execution until database is available"""
 
     def handle(self, *args, **kwargs):
         self.stdout.write('Waiting for database...')
-        db_conn=None
+        db_conn = None
         while not db_conn:
             try:
                 # Try to set db_conn variable to the db connection
@@ -19,4 +20,3 @@ class Command(BaseCommand):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
         self.stdout.write(self.style.SUCCESS('Database available!'))
-        
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,7 +1,7 @@
 # allow us to mock behavior of django get database function.
 # simulate behavior when database is available and not
 # when we run our command
-from unittest.mock import patch 
+from unittest.mock import patch
 # allow us to call the command in the source code
 from django.core.management import call_command
 # Error that django will throw when db is not available
@@ -13,17 +13,18 @@ class CommandTests(TestCase):
 
     def test_wait_for_db_ready(self):
         """Test waiting for db when db is available"""
-        # whenever the __getitem__ method is called in the block, 
-        # the code mocks/overrides the default connection handler 
+        # whenever the __getitem__ method is called in the block,
+        # the code mocks/overrides the default connection handler
         # and replace it with a mock object that returns what we want
         # and monitors the number of calls being made to it
-        with patch('django.db.utils.ConnectionHandler.__getitem__') as get_item:
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as \
+            get_item:
             # instruct this mock to always return true
-            get_item.return_value=True
+            get_item.return_value = True
             # wait_for_db is the command we will create
             call_command('wait_for_db')
             # access the call_count attribute for the object
-            self.assertEqual(get_item.call_count,1)
+            self.assertEqual(get_item.call_count, 1)
 
         # Use patch as decorator. Essentially does the same thing as above,
         # overriding the default bahavor of a function. The mock object
@@ -34,8 +35,9 @@ class CommandTests(TestCase):
         @patch('time.sleep', return_value=True)
         def test_wait_for_db(self, time_sleep):
             """Test waiting for db until the db is ready"""
-            with patch('django.db.utils.ConnectionHandler.__getitem__') as get_item:
+            with patch('django.db.utils.ConnectionHandler.__getitem__') as \
+                get_item:
                 # Make the __getitem__ method return Error the first five times
-                get_item.side_effect=[OperationalError]*5 +[True]
+                get_item.side_effect = [OperationalError]*5 +[True]
                 call_command('wait_for_db')
-                self.assertEqual(get_item.call_count,6)
+                self.assertEqual(get_item.call_count, 6)
