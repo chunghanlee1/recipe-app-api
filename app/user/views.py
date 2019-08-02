335d517aa1fe44f54626236dd75acc8e262d4a6f
chunghanlee1 <chunghan.lee1@gmail.com>
2019-08-02 11:24:36 +0800

    Added user signup, authentication, and update functionality

--- app/user/views.py
+++ app/user/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import generics, authentication, permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from user.serializers import UserSerializer, AuthTokenSerializer
 
-# Create your views here.
+
+# CreateAPIView is premade API view for serializers
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user in the system"""
+    serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for user"""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user"""
+    serializer_class = UserSerializer
+
+    # specify the mechanism by which authentication occurs
+    authentication_classes = (authentication.TokenAuthentication,)
+    permissions_classes = (permissions.IsAuthenticated,)
+
+    # override the original method which would retrieve database model
+    # instead, it uses the TokenAuthentication process to get 
+    # the user being authenticated
+    def get_object(self):
+        """Retrieve and return authenticated user"""
+        return self.request.user
